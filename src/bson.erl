f4bfe5be7a85fbbf4629c42922c0b6b9615533a5
Christian Kvalheim <christkv@gmail.com>
2012-04-17 17:14:16 +0200

    Added objectid support

--- src/bson.erl
+++ src/bson.erl
@@ -3,7 +3,7 @@
 
 % Exported functions for the bson parser
 -export ([serialize/1, deserialize/1]).
--export ([utf8/1]).
+-export ([utf8/1, regexp/2, gen_objectid/1, objectid/1, objectid/3]).
 
 % Exporting all availble types
 -export_type ([utf8/0]).
@@ -13,12 +13,7 @@
 
 % Serialize document
 serialize(Doc) ->
-	% erlang:display("-------------------------------------------------- PRE DOC"),
-	% erlang:display(Doc),
-	BinDoc = serialize_doc(Doc),
-	% erlang:display("-------------------------------------------------- POST DOC"),
-	% erlang:display(BinDoc),
-	BinDoc.
+	serialize_doc(Doc).
 	
 serialize_doc(Doc) ->
 	erlang:display("----------------------------------------------- serialize_doc"),
@@ -29,9 +24,15 @@ serialize_doc(Doc) ->
 
 serialize_doc_objects([Head|Tail]) ->	
 	case Head of
+		{Name, [{regexp, RegExp, Options}]} when is_binary(Name) ->
+			erlang:display("================================== serialize regexp"),
+			[list_to_binary([<<16#0B>>, Name, <<0>>, RegExp, <<0>>, Options, <<0>>])];
+		{Name, [{objectid, ObjectId}]} when is_binary(Name), is_binary(ObjectId) ->
+			erlang:display("================================== serialize objectid"),
+			[list_to_binary([<<16#07>>, Name, <<0>>, ObjectId])];
 		{Name, [Value]} when is_binary(Name), is_binary(Value) -> 			
 			erlang:display("================================== serialize string"),
-			[list_to_binary([<<02>>, Name, <<0>>, <<?put_int32(byte_size(Value) + 1)>>, Value, <<0>>])];
+			[list_to_binary([<<16#02>>, Name, <<0>>, <<?put_int32(byte_size(Value) + 1)>>, Value, <<0>>])];
 		{Name, [Value]} when is_binary(Name), is_float(Value) -> 			
 			erlang:display("================================== serialize float"),
 			[list_to_binary([<<16#01>>, Name, <<0>>, <<?put_float(Value)>>])];
@@ -66,12 +67,36 @@ serialize_array(_, []) -> [].
 % Deserialize document
 deserialize(BinDoc) ->
 	[].
-	
+
 %
-%	String representation
+% Utility methods
+%
+unixtime_to_secs({MegaSecs, Secs, _}) -> MegaSecs * 1000000 + Secs.
+timenow() ->
+	{MegaSecs, Secs, MicroSecs} = os:timestamp(),
+	{MegaSecs, Secs, MicroSecs div 1000 * 1000}.
+
+%@doc Fetch hostname and os pid and compress into a 5 byte id
+oid_machineprocid() ->
+	OSPid = list_to_integer (os:getpid()),
+	{ok, Hostname} = inet:gethostname(),
+	<<MachineId:3/binary, _/binary>> = erlang:md5 (Hostname),
+	<<MachineId:3/binary, OSPid:16/big>>.
+
+% Hex manipulation tools
+hexstr_to_bin(S) ->
+  hexstr_to_bin(S, []).
+hexstr_to_bin([], Acc) ->
+  list_to_binary(lists:reverse(Acc));
+hexstr_to_bin([X,Y|T], Acc) ->
+  {ok, [V], []} = io_lib:fread("~16u", [X,Y]),
+  hexstr_to_bin(T, [V | Acc]).
+
+%
+%	Types
 %
 	
-% All types used in the bson parser
+% Utf8 string representation
 -type utf8() :: unicode:unicode_binary().
 % Conversion method for string to utf8 binary, bson required utf8 strings
 -spec utf8 (unicode:chardata()) -> utf8().
@@ -80,4 +105,33 @@ utf8 (CharData) -> case unicode:characters_to_binary (CharData) of
 	{error, _Bin, _Rest} -> erlang:error (unicode_error, [CharData]);
 	{incomplete, _Bin, _Rest} -> erlang:error (unicode_incomplete, [CharData]);
 	Bin -> Bin end.
+
+% Regular expression type
+-type regexp() :: {regexp, utf8(), utf8()}.
+% Create Regular expression function
+-spec regexp (unicode:chardata(), unicode:chardata()) -> {regexp, utf8(), utf8()}.
+%@doc Convert regular expression to regexp object
+regexp (CharData, OptionData) -> {regexp, utf8(CharData), utf8(OptionData)}.
+
+% ObjectId type
+-type objectid() :: {objectid, <<_:96>>}.
+% Create objectid
+-spec objectid(integer(), <<_:40>>, integer()) -> objectid().
+%@doc Create objectid
+objectid(UnixSeconds, MachineAndProcId, Count) ->
+	{objectid, <<UnixSeconds :32/big, MachineAndProcId :5/binary, Count :24/big>>}.
+
+-spec objectid(string()) -> objectid().
+objectid(HexString) ->
+	{objectid, hexstr_to_bin(HexString)}.
+
+%@doc Fresh object id
+-spec gen_objectid(fun()) -> objectid().
+gen_objectid(CounterFun) ->
+	Now = unixtime_to_secs(timenow()),
+	MPid = oid_machineprocid(),
+	N = CounterFun(),
+	objectid (Now, MPid, N).
+
+
 
--- src/bson_tests.erl
+++ src/bson_tests.erl
@@ -2,42 +2,76 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-simple_single_level_string_doc_serialization_test() ->
-	% Build a simple string key-value doc and serialize the doc
-	Doc = orddict:append(bson:utf8("name"), bson:utf8("value"), orddict:new()),
-	BinDoc = bson:serialize(Doc),	
-	<<21,0,0,0,2,110,97,109,101,0,6,0,0,0,118,97,108,117,101,0,0>> = BinDoc.
+% simple_single_level_string_doc_serialization_test() ->
+% 	% Build a simple string key-value doc and serialize the doc
+% 	Doc = orddict:append(bson:utf8("name"), bson:utf8("value"), orddict:new()),
+% 	BinDoc = bson:serialize(Doc),	
+% 	<<21,0,0,0,2,110,97,109,101,0,6,0,0,0,118,97,108,117,101,0,0>> = BinDoc.
+% 
+% simple_single_level_float_doc_serialization_test() ->
+% 	% Build a simple float key-value doc and serialize the doc
+% 	Doc = orddict:append(bson:utf8("name"), 1.34, orddict:new()),
+% 	BinDoc = bson:serialize(Doc),
+% 	<<19,0,0,0,1,110,97,109,101,0,113,61,10,215,163,112,245,63,0>> = BinDoc.
+% 
+% simple_single_level_32_bit_integer_doc_serialization_test() ->
+% 	% Build a simple integer key-value doc and serialize the doc
+% 	Doc = orddict:append(bson:utf8("name"), 1000, orddict:new()),
+% 	BinDoc = bson:serialize(Doc),
+% 	<<15,0,0,0,16,110,97,109,101,0,232,3,0,0,0>> = BinDoc.
+% 
+% simple_single_level_64_bit_integer_doc_serialization_test() ->
+% 	% Build a simple integer key-value doc and serialize the doc
+% 	Doc = orddict:append(bson:utf8("name"), 16#7000000000000000, orddict:new()),
+% 	BinDoc = bson:serialize(Doc),
+% 	<<19,0,0,0,18,110,97,109,101,0,0,0,0,0,0,0,0,112,0>> = BinDoc.
+% 	
+% simple_two_level_document_with_values_test() ->
+% 	% Build a simple integer key-value doc and serialize the doc
+% 	InnerDoc = orddict:append(bson:utf8("integer"), 16#7000000000000000, orddict:new()),
+% 	Doc = orddict:append(bson:utf8("name"), InnerDoc, orddict:new()),
+% 	BinDoc = bson:serialize(Doc),
+% 	<<33,0,0,0,3,110,97,109,101,0,22,0,0,0,18,105,110,116,101,103,101,114,0,0,0,0,0,0,0,0,112,0,0>> = BinDoc.
+% 
+% simple_document_with_array_of_integers_test() ->
+% 	% Build a simple integer key-value doc and serialize the doc
+% 	Doc = orddict:append(bson:utf8("integers"), [1,2,3,4], orddict:new()),
+% 	BinDoc = bson:serialize(Doc),
+% 	<<48,0,0,0,4,105,110,116,101,103,101,114,115,0,33,0,0,0,16,48,0,1,0,0,0,16,49,0,2,0,0,0,16,50,0,3,0,0,0,16,51,0,4,0,0,0,0,0>> = BinDoc.
+% 	
+% simple_document_with_regexp_test() ->
+% 	% Build a simple integer key-value doc and serialize the doc
+% 	Doc = orddict:append(bson:utf8("regexp"), bson:regexp("test", "s"), orddict:new()),
+% 	BinDoc = bson:serialize(Doc),
+% 	<<20,0,0,0,11,114,101,103,101,120,112,0,116,101,115,116,0,115,0,0>> = BinDoc.
 
-simple_single_level_float_doc_serialization_test() ->
-	% Build a simple float key-value doc and serialize the doc
-	Doc = orddict:append(bson:utf8("name"), 1.34, orddict:new()),
-	BinDoc = bson:serialize(Doc),
-	<<19,0,0,0,1,110,97,109,101,0,113,61,10,215,163,112,245,63,0>> = BinDoc.
-
-simple_single_level_32_bit_integer_doc_serialization_test() ->
+simple_document_with_objectid_test() ->
+	% Generate ObjectId from hex string
+	ObjectId = bson:objectid("4f8d7e1f95f13b4370000000"),
 	% Build a simple integer key-value doc and serialize the doc
-	Doc = orddict:append(bson:utf8("name"), 1000, orddict:new()),
+	Doc = orddict:append(bson:utf8("objectid"), ObjectId, orddict:new()),
 	BinDoc = bson:serialize(Doc),
-	<<15,0,0,0,16,110,97,109,101,0,232,3,0,0,0>> = BinDoc.
-
-simple_single_level_64_bit_integer_doc_serialization_test() ->
-	% Build a simple integer key-value doc and serialize the doc
-	Doc = orddict:append(bson:utf8("name"), 16#7000000000000000, orddict:new()),
-	BinDoc = bson:serialize(Doc),
-	<<19,0,0,0,18,110,97,109,101,0,0,0,0,0,0,0,0,112,0>> = BinDoc.
-	
-simple_two_level_document_with_values_test() ->
-	% Build a simple integer key-value doc and serialize the doc
-	InnerDoc = orddict:append(bson:utf8("integer"), 16#7000000000000000, orddict:new()),
-	Doc = orddict:append(bson:utf8("name"), InnerDoc, orddict:new()),
-	BinDoc = bson:serialize(Doc),
-	<<33,0,0,0,3,110,97,109,101,0,22,0,0,0,18,105,110,116,101,103,101,114,0,0,0,0,0,0,0,0,112,0,0>> = BinDoc.
-
-simple_document_with_array_of_integers_test() ->
-	% Build a simple integer key-value doc and serialize the doc
-	Doc = orddict:append(bson:utf8("integers"), [1,2,3,4], orddict:new()),
-	BinDoc = bson:serialize(Doc),
-	<<48,0,0,0,4,105,110,116,101,103,101,114,115,0,33,0,0,0,16,48,0,1,0,0,0,16,49,0,2,0,0,0,16,50,0,3,0,0,0,16,51,0,4,0,0,0,0,0>> = BinDoc,
+	<<27,0,0,0,7,111,98,106,101,99,116,105,100,0,79,141,126,31,149,241,59,67,112,0,0,0,0>> = BinDoc,
 	erlang:display("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),
 	?debugFmt("~p~n", [BinDoc]).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
