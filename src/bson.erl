b5974a1ca76a39dcb6d8500d254e329e92babff8
Christian Kvalheim <christkv@gmail.com>
2012-04-18 11:17:00 +0200

    Dict working aswell now}

--- src/bson.erl
+++ src/bson.erl
@@ -3,7 +3,7 @@
 
 % Exported functions for the bson parser
 -export ([serialize/1, deserialize/1]).
--export ([utf8/1, regexp/2, gen_objectid/1, objectid/1, objectid/3]).
+-export ([utf8/1, regexp/2, gen_objectid/1, objectid/1, objectid/3, timestamp_to_bson_time/1, bsom_time_to_timestamp/1]).
 
 % Exporting all availble types
 -export_type ([utf8/0]).
@@ -16,45 +16,141 @@ serialize(Doc) ->
 	serialize_doc(Doc).
 	
 serialize_doc(Doc) ->
-	erlang:display("----------------------------------------------- serialize_doc"),
-	% Create final binary
-	Bin = list_to_binary(serialize_doc_objects(Doc)),
-	% Add document header
-	list_to_binary([<<?put_int32(byte_size(Bin) + 4 + 1)>>, Bin, <<0>>]).
+	% erlang:display("----------------------------------------------- serialize_doc"),
+	% erlang:display(Doc),
+
+	case Doc of
+		Doc when is_tuple(Doc), element(1, Doc) == dict ->
+			% Create final binary
+			Bin = list_to_binary(serialize_doc_objects(dict:to_list(Doc))),
+			% Add document header
+			list_to_binary([<<?put_int32(byte_size(Bin) + 4 + 1)>>, Bin, <<0>>]);
+		_ ->
+			% Create final binary
+			Bin = list_to_binary(serialize_doc_objects(Doc)),
+			% Add document header
+			list_to_binary([<<?put_int32(byte_size(Bin) + 4 + 1)>>, Bin, <<0>>])
+	end.
 
 serialize_doc_objects([Head|Tail]) ->	
-	case Head of
-		{Name, [{regexp, RegExp, Options}]} when is_binary(Name) ->
-			erlang:display("================================== serialize regexp"),
+	
+	% Handle the situation where the value is a dict
+	FinalHeadValue = case Head of
+		{HeadName, [HeadValue]} when is_tuple(HeadValue), element(1, HeadValue) == dict ->
+			% erlang:display("=================================================== DICT"),
+			{HeadName, [dict:to_list(HeadValue)]};
+		_ ->
+			% erlang:display("=================================================== NOT DICT"),
+			Head
+	end,
+
+	% erlang:display("================================================================== HEADVALUE"),
+	% erlang:display(FinalHeadValue),
+
+	% Match the case of the Head
+	case FinalHeadValue of				
+		{Name, {regexp, RegExp, Options}} when is_binary(Name) ->
+			% erlang:display("================================== serialize regexp"),
 			[list_to_binary([<<16#0B>>, Name, <<0>>, RegExp, <<0>>, Options, <<0>>])];
-		{Name, [{objectid, ObjectId}]} when is_binary(Name), is_binary(ObjectId) ->
-			erlang:display("================================== serialize objectid"),
+		{Name, {objectid, ObjectId}} when is_binary(Name), is_binary(ObjectId) ->
+			% erlang:display("================================== serialize objectid"),
 			[list_to_binary([<<16#07>>, Name, <<0>>, ObjectId])];
-		{Name, [Value]} when is_binary(Name), is_binary(Value) -> 			
-			erlang:display("================================== serialize string"),
-			[list_to_binary([<<16#02>>, Name, <<0>>, <<?put_int32(byte_size(Value) + 1)>>, Value, <<0>>])];
-		{Name, [Value]} when is_binary(Name), is_float(Value) -> 			
-			erlang:display("================================== serialize float"),
-			[list_to_binary([<<16#01>>, Name, <<0>>, <<?put_float(Value)>>])];
-		{Name, [Value]} when is_binary(Name), is_integer(Value), ?fits_int32(Value) -> 			
-			erlang:display("================================== serialize 32 bit integer"),
-			[list_to_binary([<<16#10>>, Name, <<0>>, <<?put_int32(Value)>>])];
-		{Name, [Value]} when is_binary(Name), is_integer(Value), ?fits_int64(Value) -> 			
-			erlang:display("================================== serialize 64 bit integer"),
-			[list_to_binary([<<16#12>>, Name, <<0>>, <<?put_int64(Value)>>])];
+		{Name, undefined} when is_binary(Name) ->
+			% erlang:display("================================== serialize undefined"),
+			[list_to_binary([<<16#0A>>, Name, <<0>>])];
+		{Name, null} when is_binary(Name) ->
+			% erlang:display("================================== serialize null"),
+			[list_to_binary([<<16#0A>>, Name, <<0>>])];
+		{Name, {MegaSecs, Seconds, Micro}} ->
+			% erlang:display("================================== serialize date"),
+			[list_to_binary([<<16#09>>, Name, <<0>>, <<?put_int64(timestamp_to_bson_time({MegaSecs, Seconds, Micro}))>>])];
+		{Name, Value} when is_binary(Name), is_boolean(Value) -> 			
+			% erlang:display("================================== serialize boolean"),
+			case Value of
+				true->
+					[list_to_binary([<<16#08>>, Name, <<0>>, <<1>>])];
+				_ ->
+					[list_to_binary([<<16#08>>, Name, <<0>>, <<0>>])]
+			end;
+		{Name, Value} when is_binary(Name), is_atom(Value) ->
+			% erlang:display("================================== serialize symbol"),			
+			ValueBin = atom_to_binary(Value, utf8),			
+			[list_to_binary([<<16#0E>>, Name, <<0>>, <<?put_int32(byte_size(ValueBin) + 1)>>, ValueBin, <<0>>])];
 		{Name, [[Value]]} when is_binary(Name), is_tuple(Value) -> 			
-			erlang:display("================================== serialize object"),			
+			% erlang:display("================================== serialize object"),			
 			% trigger serialization of all the values
 			[Object] = serialize_doc_objects([Value]),
 			[list_to_binary([<<16#03>>, Name, <<0>>, <<?put_int32(4 + byte_size(Object) + 1)>>, Object, <<0>>])];
 		{Name, [Value]} when is_binary(Name), is_list(Value) -> 			
-			erlang:display("================================== serialize array"),
+			% erlang:display("================================== serialize array"),
 			% Serialize the array
 			BinDoc = serialize_array(0, Value),			
 			% trigger serialization of all the values
 			[list_to_binary([<<16#04>>, Name, <<0>>, <<?put_int32(4 + byte_size(BinDoc) + 1)>>, BinDoc, <<0>>])];
+		{Name, Value} when is_binary(Name), is_binary(Value) ->
+			[list_to_binary([<<16#02>>, Name, <<0>>, <<?put_int32(byte_size(Value) + 1)>>, Value, <<0>>])];			
+		{Name, Value} when is_binary(Name), is_float(Value) -> 			
+			% erlang:display("================================== serialize float"),
+			[list_to_binary([<<16#01>>, Name, <<0>>, <<?put_float(Value)>>])];
+		{Name, Value} when is_binary(Name), is_integer(Value), ?fits_int32(Value) -> 			
+			% erlang:display("================================== serialize 32 bit integer"),
+			[list_to_binary([<<16#10>>, Name, <<0>>, <<?put_int32(Value)>>])];
+		{Name, Value} when is_binary(Name), is_integer(Value), ?fits_int64(Value) -> 			
+			% erlang:display("================================== serialize 64 bit integer"),
+			[list_to_binary([<<16#12>>, Name, <<0>>, <<?put_int64(Value)>>])];
+		{Name, Value} when is_binary(Name), is_tuple(Value) -> 			
+			% erlang:display("================================== serialize object"),			
+			% trigger serialization of all the values
+			[Object] = serialize_doc_objects([Value]),
+			[list_to_binary([<<16#03>>, Name, <<0>>, <<?put_int32(4 + byte_size(Object) + 1)>>, Object, <<0>>])];
+		{Name, [{regexp, RegExp, Options}]} when is_binary(Name) ->
+			% erlang:display("================================== serialize regexp"),
+			[list_to_binary([<<16#0B>>, Name, <<0>>, RegExp, <<0>>, Options, <<0>>])];
+		{Name, [undefined]} when is_binary(Name) ->
+			% erlang:display("================================== serialize undefined"),
+			[list_to_binary([<<16#0A>>, Name, <<0>>])];
+		{Name, [null]} when is_binary(Name) ->
+			% erlang:display("================================== serialize null"),
+			[list_to_binary([<<16#0A>>, Name, <<0>>])];
+		{Name, [{MegaSecs, Seconds, Micro}]} ->
+			% erlang:display("================================== serialize date"),
+			[list_to_binary([<<16#09>>, Name, <<0>>, <<?put_int64(timestamp_to_bson_time({MegaSecs, Seconds, Micro}))>>])];
+		{Name, [{objectid, ObjectId}]} when is_binary(Name), is_binary(ObjectId) ->
+			% erlang:display("================================== serialize objectid"),
+			[list_to_binary([<<16#07>>, Name, <<0>>, ObjectId])];
+		{Name, [Value]} when is_binary(Name), is_binary(Value) -> 			
+			% erlang:display("================================== serialize string"),
+			[list_to_binary([<<16#02>>, Name, <<0>>, <<?put_int32(byte_size(Value) + 1)>>, Value, <<0>>])];
+		{Name, [Value]} when is_binary(Name), is_boolean(Value) -> 			
+			% erlang:display("================================== serialize boolean"),
+			case Value of
+				true->
+					[list_to_binary([<<16#08>>, Name, <<0>>, <<1>>])];
+				_ ->
+					[list_to_binary([<<16#08>>, Name, <<0>>, <<0>>])]
+			end;
+		{Name, [Value]} when is_binary(Name), is_atom(Value) ->
+			% erlang:display("================================== serialize symbol"),			
+			ValueBin = atom_to_binary(Value, utf8),			
+			[list_to_binary([<<16#0E>>, Name, <<0>>, <<?put_int32(byte_size(ValueBin) + 1)>>, ValueBin, <<0>>])];
+		{Name, [Value]} when is_binary(Name), is_float(Value) -> 			
+			% erlang:display("================================== serialize float"),
+			[list_to_binary([<<16#01>>, Name, <<0>>, <<?put_float(Value)>>])];
+		{Name, [Value]} when is_binary(Name), is_integer(Value), ?fits_int32(Value) -> 			
+			% erlang:display("================================== serialize 32 bit integer"),
+			[list_to_binary([<<16#10>>, Name, <<0>>, <<?put_int32(Value)>>])];
+		{Name, [Value]} when is_binary(Name), is_integer(Value), ?fits_int64(Value) -> 			
+			% erlang:display("================================== serialize 64 bit integer"),
+			[list_to_binary([<<16#12>>, Name, <<0>>, <<?put_int64(Value)>>])];
+		{Name, [Value|_]} when is_binary(Name), is_tuple(Value) == false, is_list(Value) == false ->
+			% erlang:display("================================== serialize array"),
+			{_, ArrayValue} = Head,
+			% Serialize the array
+			BinDoc = serialize_array(0, ArrayValue),
+			% trigger serialization of all the values
+			[list_to_binary([<<16#04>>, Name, <<0>>, <<?put_int32(4 + byte_size(BinDoc) + 1)>>, BinDoc, <<0>>])];
 		_ -> 
-			erlang:display("================================== serialize done"),			
+			% erlang:display("================================== serialize done"),			
 			serialize_doc_objects(Tail)
 	end;
 serialize_doc_objects([]) -> [].
@@ -65,7 +161,7 @@ serialize_array(Number, [Head|Tail]) ->
 serialize_array(_, []) -> [].
 
 % Deserialize document
-deserialize(BinDoc) ->
+deserialize(_BinDoc) ->
 	[].
 
 %
@@ -92,6 +188,13 @@ hexstr_to_bin([X,Y|T], Acc) ->
   {ok, [V], []} = io_lib:fread("~16u", [X,Y]),
   hexstr_to_bin(T, [V | Acc]).
 
+% Timestamp conversions
+timestamp_to_bson_time({MegaSecs, Seconds, MicroSec}) ->
+	MegaSecs * 1000000 * 1000000 + Seconds * 1000000 + MicroSec.
+
+bsom_time_to_timestamp(Timestamp) ->
+	{Timestamp div 1000000000000, Timestamp div 1000000 rem 1000000, Timestamp rem 1000000}.
+
 %
 %	Types
 %
@@ -109,7 +212,7 @@ utf8 (CharData) -> case unicode:characters_to_binary (CharData) of
 % Regular expression type
 -type regexp() :: {regexp, utf8(), utf8()}.
 % Create Regular expression function
--spec regexp (unicode:chardata(), unicode:chardata()) -> {regexp, utf8(), utf8()}.
+-spec regexp (unicode:chardata(), unicode:chardata()) -> regexp().
 %@doc Convert regular expression to regexp object
 regexp (CharData, OptionData) -> {regexp, utf8(CharData), utf8(OptionData)}.
 
--- src/bson_tests.erl
+++ src/bson_tests.erl
@@ -2,48 +2,110 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-% simple_single_level_string_doc_serialization_test() ->
-% 	% Build a simple string key-value doc and serialize the doc
-% 	Doc = orddict:append(bson:utf8("name"), bson:utf8("value"), orddict:new()),
-% 	BinDoc = bson:serialize(Doc),	
-% 	<<21,0,0,0,2,110,97,109,101,0,6,0,0,0,118,97,108,117,101,0,0>> = BinDoc.
-% 
-% simple_single_level_float_doc_serialization_test() ->
-% 	% Build a simple float key-value doc and serialize the doc
-% 	Doc = orddict:append(bson:utf8("name"), 1.34, orddict:new()),
-% 	BinDoc = bson:serialize(Doc),
-% 	<<19,0,0,0,1,110,97,109,101,0,113,61,10,215,163,112,245,63,0>> = BinDoc.
-% 
-% simple_single_level_32_bit_integer_doc_serialization_test() ->
-% 	% Build a simple integer key-value doc and serialize the doc
-% 	Doc = orddict:append(bson:utf8("name"), 1000, orddict:new()),
-% 	BinDoc = bson:serialize(Doc),
-% 	<<15,0,0,0,16,110,97,109,101,0,232,3,0,0,0>> = BinDoc.
-% 
-% simple_single_level_64_bit_integer_doc_serialization_test() ->
-% 	% Build a simple integer key-value doc and serialize the doc
-% 	Doc = orddict:append(bson:utf8("name"), 16#7000000000000000, orddict:new()),
-% 	BinDoc = bson:serialize(Doc),
-% 	<<19,0,0,0,18,110,97,109,101,0,0,0,0,0,0,0,0,112,0>> = BinDoc.
-% 	
-% simple_two_level_document_with_values_test() ->
-% 	% Build a simple integer key-value doc and serialize the doc
-% 	InnerDoc = orddict:append(bson:utf8("integer"), 16#7000000000000000, orddict:new()),
-% 	Doc = orddict:append(bson:utf8("name"), InnerDoc, orddict:new()),
-% 	BinDoc = bson:serialize(Doc),
-% 	<<33,0,0,0,3,110,97,109,101,0,22,0,0,0,18,105,110,116,101,103,101,114,0,0,0,0,0,0,0,0,112,0,0>> = BinDoc.
-% 
-% simple_document_with_array_of_integers_test() ->
-% 	% Build a simple integer key-value doc and serialize the doc
-% 	Doc = orddict:append(bson:utf8("integers"), [1,2,3,4], orddict:new()),
-% 	BinDoc = bson:serialize(Doc),
-% 	<<48,0,0,0,4,105,110,116,101,103,101,114,115,0,33,0,0,0,16,48,0,1,0,0,0,16,49,0,2,0,0,0,16,50,0,3,0,0,0,16,51,0,4,0,0,0,0,0>> = BinDoc.
-% 	
-% simple_document_with_regexp_test() ->
-% 	% Build a simple integer key-value doc and serialize the doc
-% 	Doc = orddict:append(bson:utf8("regexp"), bson:regexp("test", "s"), orddict:new()),
-% 	BinDoc = bson:serialize(Doc),
-% 	<<20,0,0,0,11,114,101,103,101,120,112,0,116,101,115,116,0,115,0,0>> = BinDoc.
+simple_single_level_string_doc_serialization_test() ->
+	% Build a simple string key-value dict and serialize the doc
+	Doc = orddict:append(bson:utf8("name"), bson:utf8("value"), orddict:new()),
+	BinDoc = bson:serialize(Doc),	
+	<<21,0,0,0,2,110,97,109,101,0,6,0,0,0,118,97,108,117,101,0,0>> = BinDoc,
+	% Build a simple string key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("name"), bson:utf8("value")}],
+	BinDocProp = bson:serialize(DocProp),
+	<<21,0,0,0,2,110,97,109,101,0,6,0,0,0,118,97,108,117,101,0,0>> = BinDocProp,
+	% Build a simple string key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("name"), bson:utf8("value"), dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<21,0,0,0,2,110,97,109,101,0,6,0,0,0,118,97,108,117,101,0,0>> = BinDocDict.
+
+simple_single_level_float_doc_serialization_test() ->
+	% Build a simple float key-value dict and serialize the doc
+	Doc = orddict:append(bson:utf8("name"), 1.34, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<19,0,0,0,1,110,97,109,101,0,113,61,10,215,163,112,245,63,0>> = BinDoc,
+	% Build a simple float key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("name"), 1.34}],
+	BinDocProp = bson:serialize(DocProp),	
+	<<19,0,0,0,1,110,97,109,101,0,113,61,10,215,163,112,245,63,0>> = BinDocProp,
+	% Build a simple float key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("name"), 1.34, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<19,0,0,0,1,110,97,109,101,0,113,61,10,215,163,112,245,63,0>> = BinDocDict.
+
+simple_single_level_32_bit_integer_doc_serialization_test() ->
+	% Build a simple integer key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("name"), 1000, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<15,0,0,0,16,110,97,109,101,0,232,3,0,0,0>> = BinDoc,
+	% Build a simple integer key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("name"), 1000}],
+	BinDocProp = bson:serialize(DocProp),	
+	<<15,0,0,0,16,110,97,109,101,0,232,3,0,0,0>> = BinDocProp,
+	% Build a simple integer key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("name"), 1000, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<15,0,0,0,16,110,97,109,101,0,232,3,0,0,0>> = BinDocDict.
+
+simple_single_level_64_bit_integer_doc_serialization_test() ->
+	% Build a simple integer key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("name"), 16#7000000000000000, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<19,0,0,0,18,110,97,109,101,0,0,0,0,0,0,0,0,112,0>> = BinDoc,
+	% Build a simple integer key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("name"), 16#7000000000000000}],
+	BinDocProp = bson:serialize(DocProp),	
+	<<19,0,0,0,18,110,97,109,101,0,0,0,0,0,0,0,0,112,0>> = BinDocProp,
+	% Build a simple integer key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("name"), 16#7000000000000000, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<19,0,0,0,18,110,97,109,101,0,0,0,0,0,0,0,0,112,0>> = BinDocDict.
+	
+simple_two_level_document_with_values_test() ->
+	% Build a simple integer key-value doc and serialize the doc
+	InnerDoc = orddict:append(bson:utf8("integer"), 16#7000000000000000, orddict:new()),
+	Doc = orddict:append(bson:utf8("name"), InnerDoc, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<33,0,0,0,3,110,97,109,101,0,22,0,0,0,18,105,110,116,101,103,101,114,0,0,0,0,0,0,0,0,112,0,0>> = BinDoc,
+	% Build a simple integer key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("name"), {bson:utf8("integer"), 16#7000000000000000}}],
+	BinDocProp = bson:serialize(DocProp),	
+	<<33,0,0,0,3,110,97,109,101,0,22,0,0,0,18,105,110,116,101,103,101,114,0,0,0,0,0,0,0,0,112,0,0>> = BinDocProp,
+	% Build a simple integer key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("name"), dict:append(bson:utf8("integer"), 16#7000000000000000, dict:new()), dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<33,0,0,0,3,110,97,109,101,0,22,0,0,0,18,105,110,116,101,103,101,114,0,0,0,0,0,0,0,0,112,0,0>> = BinDocDict.
+
+simple_document_with_array_of_integers_test() ->
+	% Build a simple integer key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("integers"), [1,2,3,4], orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<48,0,0,0,4,105,110,116,101,103,101,114,115,0,33,0,0,0,16,48,0,1,0,0,0,16,49,0,2,0,0,0,16,50,0,3,0,0,0,16,51,0,4,0,0,0,0,0>> = BinDoc,
+	% Build a simple integer key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("integers"), [1,2,3,4]}],
+	BinDocProp = bson:serialize(DocProp),	
+	<<48,0,0,0,4,105,110,116,101,103,101,114,115,0,33,0,0,0,16,48,0,1,0,0,0,16,49,0,2,0,0,0,16,50,0,3,0,0,0,16,51,0,4,0,0,0,0,0>> = BinDocProp,
+	% Build a simple integer key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("integers"), [1,2,3,4], dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<48,0,0,0,4,105,110,116,101,103,101,114,115,0,33,0,0,0,16,48,0,1,0,0,0,16,49,0,2,0,0,0,16,50,0,3,0,0,0,16,51,0,4,0,0,0,0,0>> = BinDocDict.
+	
+simple_document_with_regexp_test() ->
+	% Build a simple regexp key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("regexp"), bson:regexp("test", "s"), orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<20,0,0,0,11,114,101,103,101,120,112,0,116,101,115,116,0,115,0,0>> = BinDoc,
+	% Build a simple regexp key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("regexp"), bson:regexp("test", "s")}],
+	BinDocProp = bson:serialize(DocProp),
+	<<20,0,0,0,11,114,101,103,101,120,112,0,116,101,115,116,0,115,0,0>> = BinDocProp,
+	% Build a simple regexp key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("regexp"), bson:regexp("test", "s"), dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<20,0,0,0,11,114,101,103,101,120,112,0,116,101,115,116,0,115,0,0>> = BinDocDict.
+	% erlang:display("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),
+	% ?debugFmt("~p~n", [BinDoc]),
+	% erlang:display("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),
+	% ?debugFmt("~p~n", [BinDocProp]),
+	% erlang:display("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),
+	% ?debugFmt("~p~n", [BinDocDict]).
 
 simple_document_with_objectid_test() ->
 	% Generate ObjectId from hex string
@@ -52,9 +114,89 @@ simple_document_with_objectid_test() ->
 	Doc = orddict:append(bson:utf8("objectid"), ObjectId, orddict:new()),
 	BinDoc = bson:serialize(Doc),
 	<<27,0,0,0,7,111,98,106,101,99,116,105,100,0,79,141,126,31,149,241,59,67,112,0,0,0,0>> = BinDoc,
-	erlang:display("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),
-	?debugFmt("~p~n", [BinDoc]).
+	% Build a simple objectid key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("objectid"), ObjectId}],
+	BinDocProp = bson:serialize(DocProp),
+	<<27,0,0,0,7,111,98,106,101,99,116,105,100,0,79,141,126,31,149,241,59,67,112,0,0,0,0>> = BinDocProp,
+	% Build a simple objectid key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("objectid"), ObjectId, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<27,0,0,0,7,111,98,106,101,99,116,105,100,0,79,141,126,31,149,241,59,67,112,0,0,0,0>> = BinDocDict.
 
+simple_document_with_boolean_test() ->
+	% Build a simple integer key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("bool"), true, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<12,0,0,0,8,98,111,111,108,0,1,0>> = BinDoc,
+	Doc2 = orddict:append(bson:utf8("bool"), false, orddict:new()),
+	BinDoc2 = bson:serialize(Doc2),
+	<<12,0,0,0,8,98,111,111,108,0,0,0>> = BinDoc2,
+	% Build a simple boolean key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("bool"), true}],
+	BinDocProp = bson:serialize(DocProp),
+	<<12,0,0,0,8,98,111,111,108,0,1,0>> = BinDocProp,
+	DocProp2 = [{bson:utf8("bool"), false}],
+	BinDocProp2 = bson:serialize(DocProp2),
+	<<12,0,0,0,8,98,111,111,108,0,0,0>> = BinDocProp2,
+	% Build a simple boolean key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("bool"), true, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<12,0,0,0,8,98,111,111,108,0,1,0>> = BinDocDict,
+	DocDict2 = dict:append(bson:utf8("bool"), false, dict:new()),
+	BinDocDict2 = bson:serialize(DocDict2),
+	<<12,0,0,0,8,98,111,111,108,0,0,0>> = BinDocDict2.
+
+simple_document_with_null_or_undefined_test() ->
+	% Build a simple null/undefined key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("bool"), undefined, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<11,0,0,0,10,98,111,111,108,0,0>> = BinDoc,
+	Doc2 = orddict:append(bson:utf8("bool"), null, orddict:new()),
+	BinDoc2 = bson:serialize(Doc2),
+	<<11,0,0,0,10,98,111,111,108,0,0>> = BinDoc2,
+	% Build a simple null/undefined key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("bool"), undefined}],
+	BinDocProp = bson:serialize(DocProp),
+	<<11,0,0,0,10,98,111,111,108,0,0>> = BinDocProp,
+	DocProp2 = [{bson:utf8("bool"), null}],
+	BinDocProp2 = bson:serialize(DocProp2),
+	<<11,0,0,0,10,98,111,111,108,0,0>> = BinDocProp2,
+	% Build a simple null/undefined key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("bool"), null, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<11,0,0,0,10,98,111,111,108,0,0>> = BinDocDict,
+	DocDict2 = dict:append(bson:utf8("bool"), undefined, dict:new()),
+	BinDocDict2 = bson:serialize(DocDict2),
+	<<11,0,0,0,10,98,111,111,108,0,0>> = BinDocDict2.
+
+simple_document_with_datetime_test() ->
+	% Build a simple datetime key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("date"), bson:bsom_time_to_timestamp(1334686404073), orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<19,0,0,0,9,100,97,116,101,0,233,77,130,193,54,1,0,0,0>> = BinDoc,
+	% Build a simple datetime key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("date"), bson:bsom_time_to_timestamp(1334686404073)}],
+	BinDocProp = bson:serialize(DocProp),
+	<<19,0,0,0,9,100,97,116,101,0,233,77,130,193,54,1,0,0,0>> = BinDocProp,
+	% Build a simple datetime key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("date"), bson:bsom_time_to_timestamp(1334686404073), dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<19,0,0,0,9,100,97,116,101,0,233,77,130,193,54,1,0,0,0>> = BinDocDict.
+
+simple_document_with_symbol_test() ->
+	% Build a simple symbol key-value doc and serialize the doc
+	Doc = orddict:append(bson:utf8("symbol"), atom, orddict:new()),
+	BinDoc = bson:serialize(Doc),
+	<<22,0,0,0,14,115,121,109,98,111,108,0,5,0,0,0,97,116,111,109,0,0>> = BinDoc,
+	% Build a simple symbol key-value proplist and serialize the doc
+	DocProp = [{bson:utf8("symbol"), atom}],
+	BinDocProp = bson:serialize(DocProp),
+	<<22,0,0,0,14,115,121,109,98,111,108,0,5,0,0,0,97,116,111,109,0,0>> = BinDocProp,
+	% Build a simple symbol key-value dict and serialize the doc
+	DocDict = dict:append(bson:utf8("symbol"), atom, dict:new()),
+	BinDocDict = bson:serialize(DocDict),
+	<<22,0,0,0,14,115,121,109,98,111,108,0,5,0,0,0,97,116,111,109,0,0>> = BinDocDict.
+	
 
 
 
